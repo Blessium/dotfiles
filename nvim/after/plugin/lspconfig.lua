85ae9396f19661854703f7c13cb79718df496747
Blessium <alessandro.melgar04@gmail.com>
2023-03-27 09:29:28 +0200

    Added solargraph lsp for ruby

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -18,13 +18,23 @@ local coq = require "coq"
 local lsp = require "lspconfig"
 local rust_tools = require "rust-tools"
 
+
 lsp.pyright.setup({})
 lsp.pyright.setup(coq.lsp_ensure_capabilities{
     on_attach = on_attach,
     flags = lsp_flags,
 })
 
+lsp.solargraph.setup({})
+lsp.solargraph.setup(coq.lsp_ensure_capabilities{
+    on_attach = on_attach,
+    flags = lsp_flags,
+})
+
 rust_tools.setup({})
-rust_tools.setup(coq.lsp_ensure_capabilities{})
+rust_tools.setup(coq.lsp_ensure_capabilities{
+    on_attach = on_attach,
+    flags = lsp_flags,
+})
 
 vim.cmd('COQnow --shut-up')
